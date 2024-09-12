ce91009c07732ea2d6991a73f39f31a2bf218dda
Jaiden DeChon <jaiden.dechon@gmail.com>
2024-09-11 19:57:38 -0500

    Added plugin pinia-plugin-persistedstate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,8 @@ export default defineNuxtConfig({
     compatibilityDate: "2024-04-03",
     devtools: { enabled: true },
     modules: [
-        '@pinia/nuxt',
-        '@nuxtjs/tailwindcss',
+      '@pinia/nuxt',
+      '@nuxtjs/tailwindcss',
+      'pinia-plugin-persistedstate',
     ],
 });
