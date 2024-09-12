cca5dac2887a982ceb4066c54fda21bb6987c794
Jaiden DeChon <jaiden.dechon@gmail.com>
2024-09-11 19:56:04 -0500

    Installed Nuxt instead and added pinia, tailwind

--- src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-    import ReactorStatusCard from '@/components/home/reactor-status-card.vue';
-</script>
-
-<template>
-    <div class="home-view">
-        <reactor-status-card />
-    </div>
-</template>
