0d80cd85d9d9e8e9a52258d81c50a35da2a824a9
Jaiden DeChon <jaiden.dechon@gmail.com>
2024-09-15 23:36:54 -0500

    Added desktop navbar and improved quick-controls-card

--- components/global/main-menu.vue
+++ components/global/main-menu.vue
@@ -1,76 +1,109 @@
 <script setup lang="ts">
+import { navigationMenuTriggerStyle } from '../ui';
 import { useMainMenuDrawerStore } from '~/stores/main-menu-drawer-state';
 
 const mainMenuDrawerStore = useMainMenuDrawerStore();
+
+const links: { title: string; href: string; icon: string }[] = [
+    {
+        title: 'Home',
+        href: '/',
+        icon: 'ic:round-home',
+    },
+    {
+        title: 'History',
+        href: '/history',
+        icon: 'ic:round-access-time-filled',
+    },
+];
 </script>
 
 <template>
-    <div class="border-b-2 border-border p-3 bg-background/40 backdrop-blur-lg md:px-24 lg:px-36 xl:px-72">
-        <dialog-drawer-combo>
-            <template #trigger>
-                <Button
-                    variant="ghost"
-                >
-                    <Icon
-                        name="material-symbols:menu-rounded"
-                        size="1.3em"
-                    />
-                </Button>
-            </template>
-
-            <template #body>
-                <div class="w-full p-4 flex flex-col gap-3">
-                    <!-- Home link. -->
+    <div class="border-b-2 border-border p-3 bg-background/40 backdrop-blur-lg">
+        <!-- Mobile version of navbar. -->
+        <div class="md:hidden">
+            <dialog-drawer-combo>
+                <template #trigger>
                     <Button
-                        as-child
-                        variant="secondary"
-                        class="justify-start"
+                        variant="ghost"
                     >
-                        <NuxtLink
-                            to="/"
-                            @click="mainMenuDrawerStore.closeMainMenuDrawer()"
-                        >
-                            <Icon
-                                name="ic:round-home"
-                                size="1rem"
-                                class="mr-2"
-                            />
-                            Home
-                        </NuxtLink>
+                        <Icon
+                            name="material-symbols:menu-rounded"
+                            size="1.3em"
+                        />
                     </Button>
+                </template>
 
-                    <!-- History link. -->
-                    <Button
-                        as-child
-                        variant="secondary"
-                        class="justify-start"
+                <template #body>
+                    <div class="w-full p-4 flex flex-col gap-3">
+                        <Button
+                            v-for="link in links"
+                            :key="link.title"
+                            as-child
+                            variant="secondary"
+                            class="justify-start"
+                        >
+                            <nuxt-link
+                                :to="link.href"
+                                @click="mainMenuDrawerStore.closeMainMenuDrawer()"
+                            >
+                                <Icon
+                                    :name="link.icon"
+                                    size="1rem"
+                                    class="mr-2"
+                                />
+                                {{ link.title }}
+                            </nuxt-link>
+                        </Button>
+
+                        <quick-controls-card
+                            class="my-9"
+                            @close-main-menu-drawer="mainMenuDrawerStore.closeMainMenuDrawer()"
+                        />
+                    </div>
+                </template>
+
+                <template #footer>
+                    <Button>
+                        Close
+                    </Button>
+                </template>
+            </dialog-drawer-combo>
+        </div>
+
+        <!-- Desktop version of navbar. -->
+        <div class="hidden md:flex lg:px-32">
+            <navigation-menu>
+                <navigation-menu-list>
+                    <!-- List of links. -->
+                    <navigation-menu-item
+                        v-for="link in links"
+                        :key="link.title"
                     >
-                        <NuxtLink
-                            to="/history"
-                            @click="mainMenuDrawerStore.closeMainMenuDrawer()"
-                        >
-                            <Icon
-                                name="ic:round-access-time-filled"
-                                size="1rem"
-                                class="mr-2"
-                            />
-                            History
-                        </NuxtLink>
-                    </Button>
+                        <nuxt-link :to="link.href">
+                            <navigation-menu-link
+                                :class="navigationMenuTriggerStyle()"
+                            >
+                                <Icon
+                                    :name="link.icon"
+                                    size="1rem"
+                                    class="mr-2"
+                                />
+                                {{ link.title }}
+                            </navigation-menu-link>
+                        </nuxt-link>
+                    </navigation-menu-item>
 
-                    <quick-controls-card
-                        class="my-9"
-                        @close-main-menu-drawer="mainMenuDrawerStore.closeMainMenuDrawer()"
-                    />
-                </div>
-            </template>
-
-            <template #footer>
-                <Button>
-                    Close
-                </Button>
-            </template>
-        </dialog-drawer-combo>
+                    <!-- Quick controls dropdown. -->
+                    <navigation-menu-item>
+                        <navigation-menu-trigger>Quick controls</navigation-menu-trigger>
+                        <navigation-menu-content class="w-96">
+                            <quick-controls-card />
+                        </navigation-menu-content>
+                    </navigation-menu-item>
+                </navigation-menu-list>
+            </navigation-menu>
+        </div>
     </div>
 </template>
 
--- components/global/quick-controls-card.vue
+++ components/global/quick-controls-card.vue
@@ -1,25 +1,13 @@
 <script setup lang="ts">
 import FireSuppressionButton from '~/components/reactor-controls/fire-suppression-button.vue';
 import { AugerStatusEnum } from '~/lib/constants/auger-status-enum';
+import { useReactorStateStore } from '#imports';
 
-const augerOneStatus = ref<AugerStatusEnum>(AugerStatusEnum.FORWARD);
-const augerTwoStatus = ref<AugerStatusEnum>(AugerStatusEnum.STOPPED);
-
-function pickAugerStatusIcon(augerStatus: AugerStatusEnum): string {
-    switch (augerStatus) {
-        case AugerStatusEnum.FORWARD:
-            return 'ic:round-arrow-forward';
-        case AugerStatusEnum.REVERSE:
-            return 'ic:round-arrow-back';
-        case AugerStatusEnum.STOPPED:
-        default:
-            return 'ic:round-highlight-off';
-    }
-}
+const reactorStateStore = useReactorStateStore();
 </script>
 
 <template>
-    <Card class="bg-muted">
+    <Card class="bg-muted w-full md:w-96">
         <!-- Body. -->
         <CardContent class="p-3 flex flex-col gap-6">
             <!-- Critical controls. -->
@@ -57,81 +45,33 @@ function pickAugerStatusIcon(augerStatus: AugerStatusEnum): string {
                     Kill magnetrons
                 </Button>
 
-                <!-- Barrel auger dropdown. -->
-                <dropdown-menu>
-                    <!-- Trigger button. -->
-                    <dropdown-menu-trigger as-child>
-                        <Button class="gap-3">
-                            Barrel:
-                            <Badge
-                                :variant="augerOneStatus === AugerStatusEnum.FORWARD ? 'secondary' : 'destructive'"
-                                class="capitalize"
-                            >
-                                {{ augerOneStatus }}
-                            </Badge>
-                        </Button>
-                    </dropdown-menu-trigger>
-
-                    <!-- Dropdown content. -->
-                    <dropdown-menu-content class="w-64">
-                        <dropdown-menu-label>Barrel auger control</dropdown-menu-label>
-                        <dropdown-menu-separator />
-                        <dropdown-menu-radio-group v-model="augerOneStatus">
-                            <dropdown-menu-radio-item
-                                v-for="status in Object.values(AugerStatusEnum)"
-                                :key="status"
-                                :value="status"
-                                class="capitalize cursor-pointer"
-                            >
-                                {{ status }}
-                                <Icon
-                                    :name="pickAugerStatusIcon(status)"
-                                    size="1em"
-                                    class="ml-auto"
-                                    :style="status === AugerStatusEnum.STOPPED ? 'color: red' : undefined"
-                                />
-                            </dropdown-menu-radio-item>
-                        </dropdown-menu-radio-group>
-                    </dropdown-menu-content>
-                </dropdown-menu>
+                <!-- Barrel auger toggle. -->
+                <Button
+                    class="gap-3"
+                    @click="reactorStateStore.barrelAugerStatus === AugerStatusEnum.FORWARD ? reactorStateStore.stopBarrelAuger() : reactorStateStore.barrelAugerForward()"
+                >
+                    Barrel:
+                    <Badge
+                        :variant="reactorStateStore.barrelAugerStatus === AugerStatusEnum.FORWARD ? 'secondary' : 'destructive'"
+                        class="capitalize"
+                    >
+                        {{ reactorStateStore.barrelAugerStatus }}
+                    </Badge>
+                </Button>
 
                 <!-- Intake auger dropdown. -->
-                <dropdown-menu>
-                    <!-- Trigger button. -->
-                    <dropdown-menu-trigger as-child>
-                        <Button class="gap-3">
-                            Intake:
-                            <Badge
-                                :variant="augerTwoStatus === AugerStatusEnum.FORWARD ? 'secondary' : 'destructive'"
-                                class="capitalize"
-                            >
-                                {{ augerTwoStatus }}
-                            </Badge>
-                        </Button>
-                    </dropdown-menu-trigger>
-
-                    <!-- Dropdown content. -->
-                    <dropdown-menu-content class="w-64">
-                        <dropdown-menu-label>Intake auger control</dropdown-menu-label>
-                        <dropdown-menu-separator />
-                        <dropdown-menu-radio-group v-model="augerTwoStatus">
-                            <dropdown-menu-radio-item
-                                v-for="status in Object.values(AugerStatusEnum)"
-                                :key="status"
-                                :value="status"
-                                class="capitalize cursor-pointer"
-                            >
-                                {{ status }}
-                                <Icon
-                                    :name="pickAugerStatusIcon(status)"
-                                    size="1em"
-                                    class="ml-auto"
-                                    :style="status === AugerStatusEnum.STOPPED ? 'color: red' : undefined"
-                                />
-                            </dropdown-menu-radio-item>
-                        </dropdown-menu-radio-group>
-                    </dropdown-menu-content>
-                </dropdown-menu>
+                <Button
+                    class="gap-3"
+                    @click="reactorStateStore.intakeAugerStatus === AugerStatusEnum.FORWARD ? reactorStateStore.stopIntakeAuger() : reactorStateStore.intakeAugerForward()"
+                >
+                    Intake:
+                    <Badge
+                        :variant="reactorStateStore.intakeAugerStatus === AugerStatusEnum.FORWARD ? 'secondary' : 'destructive'"
+                        class="capitalize"
+                    >
+                        {{ reactorStateStore.intakeAugerStatus }}
+                    </Badge>
+                </Button>
             </div>
         </CardContent>
     </Card>
