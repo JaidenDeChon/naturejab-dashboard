94fd652da38c37a59bc7dfbc0f5b2ca2412d5259
Jaiden DeChon <jaiden.dechon@gmail.com>
2024-09-21 16:26:59 -0500

    Reordered some dummy data

--- components/reactor-controls/configure-run.vue
+++ components/reactor-controls/configure-run.vue
@@ -2,7 +2,8 @@
 import { ref } from 'vue';
 import { PlasticTypesEnum } from '~/lib/constants/plastic-types-enum';
 
-const selectedRuntime = ref(60);
+const selectedRuntime = ref(90);
+const selectedWeight = ref(10);
 const selectedPlasticTypes = ref<Record<string, boolean>>(
     Object.values(PlasticTypesEnum).reduce((acc, key) => {
         acc[key] = false;
@@ -24,7 +25,7 @@ const togglePlasticType = (type: string, value: boolean) => {
                 v-model="selectedRuntime"
                 :min="1"
                 :max="120"
-                class="gap-2"
+                class="gap-2 lg:w-40"
             >
                 <Label for="configure-time-limit">Run reactor for: (minutes)</Label>
                 <number-field-content>
@@ -34,13 +35,28 @@ const togglePlasticType = (type: string, value: boolean) => {
                 </number-field-content>
             </number-field>
 
+            <!-- Plastic weight field. -->
+            <number-field
+                id="configure-time-limit"
+                v-model="selectedWeight"
+                class="gap-2 lg:w-40"
+            >
+                <Label for="configure-time-limit">Plastic amount: (lbs)</Label>
+                <number-field-content>
+                    <number-field-increment />
+                    <number-field-input />
+                    <number-field-decrement />
+                </number-field-content>
+            </number-field>
+
+            <!-- Plastic selection field. -->
             <div class="flex flex-col gap-2">
                 <Label>Plastic type:</Label>
                 <dialog-drawer-combo>
                     <template #trigger>
                         <Button
                             variant="outline"
-                            class="gap-3"
+                            class="gap-3 lg:w-40 justify-between"
                         >
                             Select...
                             <Icon
@@ -52,7 +68,7 @@ const togglePlasticType = (type: string, value: boolean) => {
                     </template>
 
                     <template #body>
-                        <div class="p-4 flex flex-col gap-3 max-h-72 overflow-y-auto">
+                        <div class="p-4 flex flex-col gap-3 overflow-y-auto max-h-96">
                             <div
                                 v-for="(isSelected, plasticType) in selectedPlasticTypes"
                                 :key="plasticType"
